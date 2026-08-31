fba7b472ec2e458e220b2a8c2fea040377d7b7e5
ShruthiKannappan-cmu <skannapp@andrew.cmu.edu>
2026-08-30 22:10:16 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void printMap(map<char,int> ms1){
+        for(auto x: ms1){
+            cout<<x.first<<" "<<x.second<<endl; 
+        }
+    }
+    bool checkValid(map<char,int> ms1){
+        for(auto x: ms1){
+            if(x.second!=0) return false;
+        }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        map<char,int> ms1;
+        int ns1 = s1.length();
+        int ns2 = s2.length();
+        if(ns2< ns1) return false;
+        int s1sat = 0;
+        int indl = 0;
+        int indr = 0;
+        for(int i= 0;i<ns1;i++){
+            ms1[s1[i]]++;
+        }
+        for(;indr<ns1;indr++){
+            if(ms1.find(s2[indr])!= ms1.end()){
+                ms1[s2[indr]]--;   
+            }
+        }
+        cout<<indl<<" "<<indr<<endl;
+        printMap(ms1);
+        if(checkValid(ms1)) return true;
+        while(indr<ns2){
+            char cc = s2[indl];
+            if(ms1.find(cc)!=ms1.end()){
+                ms1[cc]++;
+            }
+            if(ms1.find(s2[indr])!= ms1.end()){
+                ms1[s2[indr]]--;
+            }
+            if(checkValid(ms1)) return true;
+            cout<<indl<<" "<<indr<<endl;
+            printMap(ms1);
+            indr++;
+            indl++;
+        }
+        return false;
+
+    }
+};
